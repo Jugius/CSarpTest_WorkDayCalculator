e77119f8cebc207aeb12a46e4e998061142b28ac
Andrew Slusarew <jugius@gmail.com>
2019-04-11 13:27:28 +0300

    Implement method Calculate Execute tests

--- CSharpTest/WorkDayCalculator.cs
+++ CSharpTest/WorkDayCalculator.cs
@@ -10,7 +10,41 @@ namespace CSharpTest
     {
         public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
         {
-            throw new NotImplementedException();
+            if (dayCount < 0) throw new ArgumentOutOfRangeException("dayCount");
+            if (dayCount <= 1) return startDate;
+            if (weekEnds == null || weekEnds.Length == 0) return startDate.AddDays(dayCount - 1);
+
+            int workingDays = 1;
+            DateTime checkingDay = startDate;
+
+            while (workingDays < dayCount)
+            {
+                checkingDay = checkingDay.AddDays(1);
+                if (!IsWeekEnd(checkingDay, weekEnds))
+                {
+                    workingDays++;
+                }
+            }
+            return checkingDay;
+        }
+        /// <summary>
+        /// Checks if DateTime is a weekend
+        /// </summary>
+        private bool IsWeekEnd(DateTime day, WeekEnd[] weekEnds)
+        {
+            //using Linq
+            //return weekEnds.FirstOrDefault(a => day >= a.StartDate && day <= a.EndDate) != null;
+
+            bool result = false;
+            foreach (var weekEnd in weekEnds)
+            {
+                if (day >= weekEnd.StartDate && day <= weekEnd.EndDate)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
         }
     }
 }
