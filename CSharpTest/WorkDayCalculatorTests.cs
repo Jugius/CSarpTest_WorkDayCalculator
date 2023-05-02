f77bbebcef541f1eee9f41b24e6086061d739991
Andrew Slusarev <jugius@gmail.com>
2023-05-02 17:49:50 +0300

    Update tests

--- CSharpTest/WorkDayCalculatorTests.cs
+++ CSharpTest/WorkDayCalculatorTests.cs
@@ -49,19 +49,75 @@ namespace CSharpTest
             Assert.IsTrue(result.Equals(new DateTime(2017, 4, 28)));
         }
         [TestMethod]
-        public void TestIncorrectRangeDaysCount()
+        public void TestStartDayIsWeekend()
         {
-            DateTime startDate = new DateTime(2014, 12, 1);
-            int count = -1;
+            DateTime startDate = new DateTime(2022, 4, 15);
+            int count = 3;
 
-            try
+            WeekEnd[] weekends = new WeekEnd[1]
             {
-                DateTime result = new WorkDayCalculator().Calculate(startDate, count, null);
-            }
-            catch (ArgumentOutOfRangeException ex)
+                new WeekEnd(new DateTime(2022, 4, 13), new DateTime(2022, 4, 17))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2022, 4, 20)));
+        }
+        [TestMethod]
+        public void TestLastDayIsWeekend()
+        {
+            DateTime startDate = new DateTime(2022, 4, 20);
+            int count = 3;
+
+            WeekEnd[] weekends = new WeekEnd[1]
             {
-                StringAssert.Contains(ex.Message, "dayCount");
-            }            
-        }      
+                new WeekEnd(new DateTime(2022, 4, 22), new DateTime(2022, 4, 23)),
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2022, 4, 24)));
+        }
+
+        [TestMethod]
+        public void TestDurationIsOneDay()
+        {
+            DateTime startDate = new DateTime(2022, 4, 20);
+            int count = 1;
+
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2022, 4, 22), new DateTime(2022, 4, 23)),
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2022, 4, 20)));
+        }
+        [TestMethod]
+        public void TestDurationIsOneDayAndStartDayIsWeekend()
+        {
+            DateTime startDate = new DateTime(2022, 4, 24);
+            int count = 1;
+
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2022, 4, 22), new DateTime(2022, 4, 25)),
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2022, 4, 26)));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestDurationIsZeroThrows()
+        {
+            DateTime startDate = new DateTime(2022, 4, 20);
+            int count = 0;
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, null);
+        }
     }
 }
